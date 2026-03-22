23004a7bad424d126f833e1d083812b4f877f714
Yoel178 <yoelbulaciavaca178@gmail.com>
2026-03-21 20:21:52 -0400

    revalidacion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from DrissionPage import ChromiumPage, ChromiumOptions
+from DrissionPage import ChromiumPage
 from config import USER, PASSWORD, LINK_PAGE
 from scraper.login import login_page, navigation_page
 from scraper.extractor import obtain_groups
@@ -8,15 +8,7 @@ from services.telegram_bot import send_message
 
 def main():
 
-    opciones = ChromiumOptions()
-        
-    # Le decimos la ruta exacta del Chrome que viene en GitHub
-    opciones.set_browser_path('/usr/bin/google-chrome')
-    
-    opciones.set_argument('--no-sandbox')
-    opciones.set_argument('--disable-dev-shm-usage')
-
-    page = ChromiumPage(opciones)
+    page = ChromiumPage()
 
     # Inciar sesion y navegar hasta el maestro de oferta
     login_page(page, USER, PASSWORD, LINK_PAGE)
